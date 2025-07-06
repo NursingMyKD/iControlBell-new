5265f7ac4b7d228e97114fb72abf976038c8c55c
Shane Stone <Medicstone4@gmail.com>
2025-07-06 19:33:48 -0400

    Initial Commit

---
+++ iControlBell/iControlBellApp.swift
@@ -0,0 +1,17 @@
+//
+//  iControlBellApp.swift
+//  iControlBell
+//
+//  Created by shane stone on 7/6/25.
+//
+
+import SwiftUI
+
+@main
+struct iControlBellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
